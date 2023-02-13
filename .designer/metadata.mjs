f46877bd227b8bb9931641b5d6e43f175a7f504d
turrsis <turrsis@gmail.com>
2023-02-13 14:41:38 +0200

    fix:: add vdsg-metadata-tests

--- .designer/metadata.mjs
+++ .designer/metadata.mjs
@@ -1,8 +1,10 @@
+import metadataTests from 'vdsg-metadata-tests'
 import metadataConfigs from 'vdsg-metadata-configs'
 import templates from './metadata.templates.mjs'
 
 export default [
     metadataConfigs,
+    metadataTests,
     {
         templates: templates
     }
