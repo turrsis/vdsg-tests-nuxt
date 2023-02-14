8e988404e6bd15d0b28e662184965541e7e4db0d
turrsis <turrsis@gmail.com>
2023-02-14 09:46:29 +0200

    refactor:: add page

--- .designer/config.mjs
+++ .designer/config.mjs
@@ -1,8 +1,17 @@
 export default {
     key: 'vdsg-tests-nuxt',
     label: 'nuxt tests',
-    tests: {
-        uiSpecsPath: '.designer-tests/.spec',
+    app: {
+        layoutsPath: 'layouts',
+        pages: {
+            pathPrefix: 'pages/',
+        },
+        router: {
+            entryPoint: undefined,
+        },
+        tests: {
+            uiSpecsPath: '.designer-tests/.spec',
+        },
     },
     originhost: 'http://localhost:8080',
     server: {
---
+++ app/router.options.js
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/guide/directory-structure/pages#custom-routes
+export default {
+  routes: (_routes) => {
+    return _routes
+  },
+}
