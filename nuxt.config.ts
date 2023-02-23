ba27913621be458bba995f227401eb362b8792fa
turrsis <turrsis@gmail.com>
2023-02-23 14:19:23 +0200

    update:: nuxt: "3.2.2"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ const { __dirname } = createCommonJS(import.meta.url) */
 
 export default async () => defineNuxtConfig(await configureSlaveConfig({
   ssr: !!env.IS_SSR,
+  //builder: 'webpack', // see - https://github.com/nuxt/framework/pull/2812
   /*vite: {
     server: {
       watch: { usePolling: true }
